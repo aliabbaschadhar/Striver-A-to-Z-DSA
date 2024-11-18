a926c1bc8045763fb066d2b776c6d64bba667534
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2024-11-19 03:23:34 +0500

    add test cases

--- 03_basicMath/02_reverse.cpp
+++ 03_basicMath/02_reverse.cpp
@@ -4,12 +4,26 @@ using namespace std;
 int reverseNumber(int num)
 {
     int revnumber = 0;
+    // For negative numbers
+    bool is_negative = num < 0;
+    num = abs(num); // Making it positive
     while (num > 0)
     {
         int lastDigit = num % 10;
         revnumber = (revnumber * 10) + lastDigit;
         num = num / 10;
     }
+    // Making +ve again -ve again if it was -ve at first
+    if (is_negative)
+    {
+        revnumber = -revnumber;
+    }
+    // Checking for overflow and underflow
+    if (revnumber > INT_MAX || revnumber < INT_MIN)
+    {
+        return 0;
+    }
+
     return revnumber;
     // TC--> O(log10N) space-->O(1)
 }
