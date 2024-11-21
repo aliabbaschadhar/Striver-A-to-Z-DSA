19d38d1ac62785856000f0f14dc4d3a4113cadee
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2024-11-21 17:57:33 +0500

    a small change in the reverseNumber func

--- 03_basicMath/02_reverse.cpp
+++ 03_basicMath/02_reverse.cpp
@@ -10,23 +10,24 @@ int reverseNumber(int num)
     while (num > 0)
     {
         int lastDigit = num % 10;
-        revnumber = (revnumber * 10) + lastDigit;
+        revnumber = (revnumber * 10) + lastDigit; // reversing a number
         num = num / 10;
+        // Checking for overflow and underflow
+        if (revnumber > INT_MAX || revnumber < INT_MIN)
+        {
+            return 0;
+        }
     }
     // Making +ve again -ve again if it was -ve at first
     if (is_negative)
     {
         revnumber = -revnumber;
     }
-    // Checking for overflow and underflow
-    if (revnumber > INT_MAX || revnumber < INT_MIN)
-    {
-        return 0;
-    }
 
     return revnumber;
     // TC--> O(log10N) space-->O(1)
 }
+
 int main()
 {
     int n = 480900;
